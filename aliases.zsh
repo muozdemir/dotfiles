ac491690a448b35f2f966dc17267ba9dee0095da
Daniel Harbor <harbord2@gmail.com>
2020-12-21 14:24:19 -0800

    Update pull and upload alias

--- aliases.zsh
+++ aliases.zsh
@@ -153,9 +153,9 @@ pas () {
 
 pau () {
   cd ~/bt/in-store
-  ./pull_from_cpair_and_upload.sh $1 192.168.86.168
+  ./pull_from_cpair_and_upload.sh $1 192.168.86.$2
   if [ $? -eq 0 ]; then
-    load-usb && say -v Daniel upload complete
+    say -v Daniel upload complete
   else
     say -v Daniel pull failed
   fi
